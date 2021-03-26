f7c340263a9dba8a3b5cc21c3ca9f5a1592e8f52
Esteban Ordonez <estebanusco@gmail.com>
2021-03-25 22:03:44 -0700

    Fully Functional Game, Organized, ready for UI leaderboard implement

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 //initial state
 let gameState = {
-  player: {
-    name: "",
-    points: 0,
-  },
   canvas: [
     [
       "",
@@ -966,14 +962,18 @@ let gameState = {
       "",
     ],
   ],
-  
 };
 
+
+// global variables
+let scores = [];
 let startGame;
 let snakeCrashed, snakeOutOfBounds;
 let newApple, newSnakeHead;
 let scoreCounter = 0;
 
+
+//the initial apple, snake and snake's direction
 let snake = {
   body: [
     [15, 13],
@@ -986,11 +986,15 @@ let snake = {
   apple: [10, 5],
 };
 
+
+//life Span functions
 function buildInitialState() {
+  fetchScores();
   renderState();
   buildApple();
   buildSnake();
 }
+
 
 // render the state
 function renderState() {
@@ -1006,6 +1010,8 @@ function renderState() {
   });
 }
 
+
+// Creates the snake and calls a function creating a new snake head with a new direction
 function buildSnake() {
   $(".segment").removeClass("snake");
 
@@ -1024,6 +1030,7 @@ function buildSnake() {
 }
 
 
+//Builds food and place it randomly without being placed in the same space as the snake
 function buildApple() {
   $(".segment").removeClass("apple");
 
@@ -1040,9 +1047,20 @@ function buildApple() {
   segmentElemApple = $(
     `[data-x="${appleCoordinateX}"][data-y="${appleCoordinateY}"]`
   );
-  segmentElemApple.hasClass('snake') ? buildApple() : segmentElemApple.addClass("apple");
+  segmentElemApple.hasClass("snake") ? buildApple() : segmentElemApple.addClass("apple");
 }
 
+
+//Gives a random number to buidApple
+function getRandomNumber(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+//Builds a new snake head for buildSnake()
 function buildNewSnakeHead() {
   const snakeHead = snake.body[0];
   const snakeheadX = snakeHead[0];
@@ -1055,6 +1073,55 @@ function buildNewSnakeHead() {
   return newSnakeHead;
 }
 
+
+//Checks if the snake head eats an apple
+function checkSnakeAte() {
+  let equals =
+    newApple.length == newSnakeHead.length &&
+    newApple.every(function (element, index) {
+      return element === newSnakeHead[index];
+    });
+  equals && hasEaten();
+}
+
+
+//If snake ate apple it will grow, build and place a new apple and add 100 to the score
+function hasEaten() {
+  snake.body.push(newApple);
+  buildApple();
+  addToScore();
+}
+
+
+//add 100 to score an update UI
+function addToScore() {
+  scoreCounter += 100;
+  
+  $('.score').text(`Your current score is: ${scoreCounter}`)
+}
+
+
+//When gameover resets the score to 0
+function resetScore(){
+  scoreCounter = 0;
+  $('.score').text(`Your current score is: ${scoreCounter}`)
+}
+
+
+// Checks if snake crashed on itself or against bounds. If so open prompt for leaderboards
+function checkGameOver() {
+  checkSnakeCrashed()
+  checkSnakeOutOfBounds()
+  
+  if (snakeCrashed === true || snakeOutOfBounds === true){
+    clearInterval(startGame)
+    $(".modal").addClass("open");
+    
+  }
+}
+
+
+//Helper Function to determine Game Over
 function checkSnakeCrashed() {
   const snakeBody = snake.body.slice(1);
 
@@ -1065,6 +1132,8 @@ function checkSnakeCrashed() {
   
 }
 
+
+//Helper Function to determine Game Over
 function checkSnakeOutOfBounds(){
    const headX  = newSnakeHead[0]
    const headY  = newSnakeHead[1]
@@ -1075,64 +1144,106 @@ function checkSnakeOutOfBounds(){
    }
 }
 
-function checkSnakeAte() {
-  let equals =
-    newApple.length == newSnakeHead.length &&
-    newApple.every(function (element, index) {
-      return element === newSnakeHead[index];
-    });
-  equals && hasEaten();
+
+// sorts scores in descending order, caps leaderboard to 3 highest scores and store scores in local storage
+function storeScore(){
+  scores.sort(function(a, b){
+    return b.score - a.score
+  })
+
+  scores.length > 3 && scores.splice(3)
+
+  localStorage.setItem("scores", JSON.stringify(scores))
 }
 
-function hasEaten() {
-  snake.body.push(newApple);
-  buildApple();
-  updateScore();
+
+// Retrieves scores from local Storage and update global variable holding the scores. If local storage null sets default leaderboard
+function fetchScores(){
+  scores = (localStorage.getItem("scores") === null ? fetchDefaultScores() : JSON.parse(localStorage.getItem("scores")));
 }
 
-function getRandomNumber(min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
 
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+//defaul leaderboard object
+function fetchDefaultScores(){
+  scores = [
+    {
+      name: "Your Name", 
+      score: 0
+    }
+  ]  
+  return scores
 }
 
-function updateScore() {
-  scoreCounter += 100;
-  console.log(scoreCounter)
-  
-  $('.score').text(`Your current score is: ${scoreCounter}`)
- 
-}
 
-function checkGameOver() {
-  checkSnakeCrashed()
-  checkSnakeOutOfBounds()
-  
-  if (snakeCrashed === true || snakeOutOfBounds === true){
-    clearInterval(startGame)
-
-  }
-}
-
+// Interval: draws the snake in canvas and it's direction, checks for Game Over and if snake ate apple
 function tick() {
   buildSnake();
   checkGameOver()
   checkSnakeAte() 
 }
 
+
+// starts the game loop
 function gameOn(){
     startGame = setInterval(tick, 100);
     return startGame
 }
 
+
+//Event Handlers
+
+
+//on load start the game automatically - better be ready
 $(window).on("load", function (event){
     if(startGame === undefined || startGame === null){
+      buildInitialState()
       gameOn()
     }
 })
 
 
+//Save player's  name and score
+$('.save-score').click(function(event){
+  event.preventDefault()
+  let playerName = $('#player-name').val()
+  let finalScore = scoreCounter;
+  
+  let scoreObj =  { 
+      name: playerName, 
+      score: finalScore,
+    } 
+  
+  scores.push(scoreObj)
+  storeScore()
+  $(".gameOver-form").trigger("reset");
+  
+})
+
+
+// starts a new game reseting the snake, global variables and score
+$('.start-new-game').click(function(event){
+  event.preventDefault();
+  resetScore()
+  $('.gameOver-form').trigger("reset");
+  snakeCrashed = false;
+  snakeOutOfBounds = false;
+  snake = {
+    body: [
+      [15, 13],
+      [15, 14],
+      [15, 15],
+      [15, 16],
+      [15, 17],
+    ],
+    nextDirection: [0, -1],
+    apple: [],
+  };
+  $(".modal").removeClass("open");
+  gameOn()
+})
+
+
+//Gives snake it's directions and prevents to run backwards
 $(window).on("keydown", function (event) {
   //left
   if (event.keyCode === 37 && snake.nextDirection[1] !== 1) {
@@ -1152,28 +1263,25 @@ $(window).on("keydown", function (event) {
   }
 });
 
-// function requestPlayerName() {
-  // const form = $(
-    // `<form id="playerNameForm"><input id='player-name-input type='text'><button>Save Name</button></form>`
-  // );
-  // const playerNameElem = $("#player-name");
-  // playerNameElem.append(form);
-// 
-  // $("#app").on("input", "#player-name-input", function (event) {
-    // event.preventDefault();
-    // let name = $("#player-name-input").val();
-// 
-    // gameState.player.name = name;
-    // console.log(gameState.player.name);
-  // });
-// 
-  // $("#app").on("submit", "#playerNameForm", function (event) {
-    // event.preventDefault();
-    // if (gameState.player.name.trim() !== "") {
-      // $("#player-name-input").remove();
-      // playerNameElem.text(gameState.player.name);
-    // }
-  // });
-//}
 
-buildInitialState();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
